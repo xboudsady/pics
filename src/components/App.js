912ae4435b04f0f21e5e981e1aef4ab6dccb8c16
xboudsady <boudsadyx@gmail.com>
2019-08-05 17:03:54 +0200

    Added Semantic UI, along with styling for the <SearchBar />.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 const App = () => {
-  return <div><SearchBar /></div>
-}
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar />
+    </div>
+  );
+};
 
 export default App
