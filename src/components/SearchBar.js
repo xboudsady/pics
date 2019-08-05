3612bf749a482ca3acf3f412f34b2fac6ea0713e
xboudsady <boudsadyx@gmail.com>
2019-08-05 20:14:05 +0200

    Refeactored SearchBar class, to include state. Modified onChange to update state prop.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
-  // Method called when text is changed in search field
-  // Event object as parameter
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' }
 
   render() {
     return (
@@ -16,7 +11,7 @@ class SearchBar extends React.Component {
             <label>Image Search</label>
             {/* This callback will be invoke each time a user types */}
             {/* Ommits the onInputChange() parentheses to not call when component is rendered */}
-            <input type="text" onChange={this.onInputChange} /> 
+            <input type="text" value={ this.state.term } onChange={ (e) => this.setState({ term: e.target.value }) } /> 
           </div>
         </form>
       </div>
