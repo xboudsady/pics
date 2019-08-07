a138d47b78f5871290c072f6cdad4ba220c8bf26
xboudsady <boudsadyx@gmail.com>
2019-08-07 22:27:59 +0200

    Fix Image thumbnail height difference.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,9 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { spans: 0 };
+
     // Referencing a single DOM element
     this.imageRef = React.createRef();
   }
@@ -14,19 +17,19 @@ class ImageCard extends React.Component {
 
   // When rendering, assign a 'grid-row-end' to make sure the image takes up the appropriate space
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   }
   
   render() {
     const { description, urls } = this.props.image;
     
     return (
-      <div>
-        <img 
-          ref={ this.imageRef }
-          alt={ description }
-          src={ urls.regular }
-        />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={ this.imageRef } alt={ description } src={ urls.regular } />
       </div>
     );
   }
