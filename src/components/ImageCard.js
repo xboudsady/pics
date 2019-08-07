0f1c0bf82a8cf4ef8eb538827d6dbb6012302d24
xboudsady <boudsadyx@gmail.com>
2019-08-07 22:10:25 +0200

    Added React refrence for image element,for image height.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    // Referencing a single DOM element
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // calls after image is loaded on-screen
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  // When rendering, assign a 'grid-row-end' to make sure the image takes up the appropriate space
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+  
   render() {
     const { description, urls } = this.props.image;
+    
     return (
       <div>
         <img 
+          ref={ this.imageRef }
           alt={ description }
           src={ urls.regular }
         />
